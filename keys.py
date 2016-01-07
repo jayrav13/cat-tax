677f11ba7117c14bc6b05bcdcbac2e641f1d747b
Jay Ravaliya <jayrav13@gmail.com>
2016-01-06 20:24:49 -0500

    Fixed mysql key

--- keys.py
+++ keys.py
@@ -10,7 +10,7 @@ DB_NAME="cat_tax"
 if not os.environ.get('IS_HEROKU'):
 	MYSQL_KEY="mysql://" + DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_NAME
 else:
-	MYSQL_KEY=os.environ.get('CLEARDB_DATABASE_URL')
+	MYSQL_KEY=os.environ.get('CLEARDB_DATABASE_URL').strip('?reconnect=true')
 
 # API Keys
 GoogleAPI=""
