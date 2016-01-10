bc796591f3c1c146e78e58fad469b0b91ad644b7
Jay Ravaliya <jayrav13@gmail.com>
2016-01-09 19:10:59 -0500

    Testing only at 830am, moved old files

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from twilio.rest import TwilioRestClient
 import os
 import datetime
 
-if datetime.datetime.now().hour == 8 or True:
+if datetime.datetime.now().hour == 8:
 	client = TwilioRestClient(os.environ.get('TWILIO_ACCOUNT_SID'), os.environ.get('TWILIO_AUTH_TOKEN'))
 	message = client.messages.create(to="+19738967552", from_="+19733214779", body="This works!", media_url=['http://i.imgur.com/VKitoJlh.gif'])
-	#message = client.messages.create(to="+19738967552", from_="+19733214779", body="This works!")
